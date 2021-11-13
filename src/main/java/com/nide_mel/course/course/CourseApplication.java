cd9c20e51db0bfb33e4a24e82505c42ad80fbe2a
Nicolas Tito <mrnicolastito@icloud.com>
2021-11-13 18:46:37 +0000

    create instance

--- src/main/java/com/nide_mel/course/course/CourseApplication.java
+++ src/main/java/com/nide_mel/course/course/CourseApplication.java
@@ -1,13 +1,31 @@
 package com.nide_mel.course.course;
 
+import java.util.Arrays;
+
+import com.nide_mel.course.course.domain.Category;
+import com.nide_mel.course.course.repositories.CategoryRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CourseApplication {
+public class CourseApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Computing");
+		Category cat2 = new Category(null, "Office");
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
