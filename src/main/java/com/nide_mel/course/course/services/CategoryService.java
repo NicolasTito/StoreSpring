ca9063062a31443825f2a682b3417121a478a7ff
Nicolas Tito <mrnicolastito@icloud.com>
2021-11-13 20:37:15 +0000

    message exception not found

--- src/main/java/com/nide_mel/course/course/services/CategoryService.java
+++ src/main/java/com/nide_mel/course/course/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import com.nide_mel.course.course.domain.Category;
 import com.nide_mel.course.course.repositories.CategoryRepository;
+import com.nide_mel.course.course.services.exceptions.ObjectNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,7 +17,8 @@ public class CategoryService {
 
 	public Category find(Integer id) {
 		Optional<Category> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+					"Object not found!! Id: " + id + ", Tipo: " + Category.class.getName()));
 	}
 
 }
