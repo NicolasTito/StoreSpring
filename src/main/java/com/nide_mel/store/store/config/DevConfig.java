f2dbfae891368820de73720579e0bdadd197990e
Nicolas Tito <mrnicolastito@icloud.com>
2021-11-17 10:09:55 +0000

    prepare dev properties

---
+++ src/main/java/com/nide_mel/store/store/config/DevConfig.java
@@ -0,0 +1,25 @@
+package com.nide_mel.store.store.config;
+
+import java.text.ParseException;
+
+import com.nide_mel.store.store.services.DBService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+
+	@Bean
+	public boolean instantiateDataBase() throws ParseException {
+
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+}
